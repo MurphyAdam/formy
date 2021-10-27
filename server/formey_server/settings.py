0d7d4a4424af696d013316720b5e6b6be11efeba
Adam Clark <elm.majidi@gmail.com>
2021-10-27 11:55:45 +0100

    updated settings for prod

--- server/formey_server/settings.py
+++ server/formey_server/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
